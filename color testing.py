a4bc6e55f909d214b364f0c7c02f02a6dda06481
niks <nikarora1111@gmail.com>
2018-07-30 20:15:03 +0530

    reset color range

--- color testing.py
+++ color testing.py
@@ -1,14 +1,12 @@
 import cv2
 import numpy as np
-
-
 imgpath="C:\\Users\\ARORA\\Desktop\\standard_test_images\\peppers_color.tif"
 img = cv2.imread(imgpath)
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-lower_red1 = np.array([165,100,100])
+lower_red1 = np.array([170,100,100])
 upper_red1 = np.array([179,255,255])
 lower_red2 = np.array([0,100,100])
-upper_red2= np.array([15,255,255])
+upper_red2= np.array([10,255,255])
 lower_mask = cv2.inRange(hsv, lower_red1, upper_red1)
 upper_mask = cv2.inRange(hsv, lower_red2, upper_red2)
 mask=lower_mask+upper_mask
